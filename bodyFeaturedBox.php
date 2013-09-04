186cc0b3af12e98f7130e1a5ad8155c66b0fbdaa
Nick Pettazzoni <pettazz@gmail.com>
2013-09-04 12:16:11 -0400

    componentize the featuredbox

---
+++ bodyFeaturedBox.php
@@ -0,0 +1,57 @@
+                <div id="featuredbox" class="pull-left">
+                <?php
+                    $titles = array();
+                    for($iter = 1; $iter < 5; $iter++){
+                        if($iter == 1){
+                            $active = ' active';
+                        }else{
+                            $active = '';
+                        }
+                        $post = $JACKED->Syrup->Blag->findOne(array('guid' => $templateVars['featuredBox']['posts'][$iter]));
+                        $titles[$iter] = array('title' => $post->title, 'type' => strtolower($post->category->name));
+                        //find the post image
+                        $tagExists = !(strpos($post->content, '<img class="headliner ') === FALSE);
+                        if($tagExists){
+                            $postImageTag = strstr($post->content, '/>', TRUE) . '/>';
+                            $srcpos = strpos($postImageTag, 'src="/') + 6;
+                            $quotpos = strpos($postImageTag, '"', $srcpos);
+                            $postImage = '/' . substr($postImageTag, $srcpos, $quotpos - $srcpos);
+                        }else{
+                            $postImage = '/assets/ico/favicon_src/wnv_512.png';
+                        }
+                        
+                ?>
+                    <div class="item<?php echo $active; ?>" data-itemid="<?php echo $iter; ?>">
+                        <a href="/post/<?php echo $post->guid; ?>">
+                            <div class="content-preview pull-left">
+                                <h3><?php echo $post->title; ?></h3>
+                                <p class="byline">
+                                    <span class="label <?php echo strtolower($post->category->name); ?>"><?php echo $post->category->name; ?></span> <span class="datestamp"><?php echo date("F j, Y", $post->posted); ?></span>
+                                </p>
+                                <p><?php echo $post->headline; ?></p>
+                            </div>
+                            <div class="image-preview pull-right">
+                                <img src="<?php echo $postImage; ?>" />
+                            </div>
+                        </a>
+                    </div>
+                <?php
+                    }   
+                ?>
+                
+                    <div id="featured-nav">
+                        <div class="system-hr"></div>
+                        <ul class="titletabs">
+                            <?php
+                                for($iter = 1; $iter < 5; $iter++){
+                                    if($iter == 1){
+                                        $active = ' active';
+                                    }else{
+                                        $active = '';
+                                    }
+                            echo '<li class="' . $titles[$iter]['type'] . $active . '" data-itemid="' . $iter . '">' . $titles[$iter]['title'] . '</li> ' . "\n";
+                                }
+                            ?>
+                        </ul>
+                    </div>
+                </div>
--- index.php
+++ index.php
@@ -17,81 +17,16 @@
         $templateVars['postGrid']['class'] = '';
         $templateVars['postGrid']['title'] = 'RECENT POSTS';
 
+        $templateVars['featuredBox'] = array();
+        $templateVars['featuredBox']['posts'] = array(
+            1 => '031a4',
+            2 => '8024a',
+            3 => '832a3',
+            4 => '031a4'
+        );
+
         include('bodyTop.php');
-?>
-            
-                <div id="featuredbox" class="pull-left">
-                    <div class="item active" data-itemid="1">
-                        <a href="/post.htm">
-                            <div class="content-preview pull-left">
-                                <h3>Dunkin Donuts vs. Starbucks</h3>
-                                <p class="byline">
-                                    <span class="label articles">Article</span> <span class="datestamp">March 28, 2012</span>
-                                </p>
-                                <p>This is where we would put a brief intro or teaser type thing of the article to convince people that it's cool and they should read it. The container won't expand dynamically at all, so these should overall be kept relatively short. Because otherwise the text will overflow down into the tabs below, and my shiny css will actually just truncate it at an awkward point. </p>
-                            </div>
-                            <div class="image-preview pull-right">
-                                <img src="/menino_full.png" data-at2x="/menino_full@2x.png" />
-                            </div>
-                        </a>
-                    </div>
-
-                    <div class="item" data-itemid="2">
-                        <a href="/post.htm">
-                            <div class="content-preview pull-left">
-                                <h3>The Night Before Fistmas</h3>
-                                <p class="byline">
-                                    <span class="label comics">Comic</span> <span class="datestamp">December 25, 2012</span>
-                                </p>
-                                <p>Oh god why. </p>
-                            </div>
-                            <div class="image-preview pull-right">
-                                <img src="/menino_full.png" data-at2x="menino_full@2x.png" />
-                            </div>
-                        </a>
-                    </div>
-
-                    <div class="item" data-itemid="3">
-                        <a href="/post.htm">
-                            <div class="content-preview pull-left">
-                                <h3>Boston Mayor Thomas M. Menino</h3>
-                                <p class="byline">
-                                    <span class="label articles">Article</span> <span class="datestamp">March 28, 2013</span>
-                                </p>
-                                <p>This is where we would put a brief intro or teaser type thing of the article to convince people that it's cool and they should read it. The container won't expand dynamically at all, so these should overall be kept relatively short. Because otherwise the text will overflow down into the tabs below, and my shiny css will actually just truncate it at an awkward point. </p>
-                            </div>
-                            <div class="image-preview pull-right">
-                                <img src="/menino_full.png" data-at2x="menino_full@2x.png" />
-                            </div>
-                        </a>
-                    </div>
-
-                    <div class="item" data-itemid="4">
-                        <a href="/post.htm">
-                            <div class="content-preview pull-left">
-                                <h3>The Large Hadron Collider</h3>
-                                <p class="byline">
-                                    <span class="label videos">Video</span> <span class="datestamp">May 22, 2012</span>
-                                </p>
-                                <p>More like Large Hardon Collider amirite </p>
-                            </div>
-                            <div class="image-preview pull-right">
-                                <img src="/lhc.png" data-at2x="lhc@2x.png" />
-                            </div>
-                        </a>
-                    </div>
-                    <div id="featured-nav">
-                        <div class="system-hr"></div>
-                        <ul class="titletabs">
-                            <li class="articles active" data-itemid="1">Dunkin Donuts vs. Starbucks</li>
-                            <li class="comics" data-itemid="2">The Night Before Fistmas</li>
-                            <li class="articles" data-itemid="3">Boston Mayor Thomas M. Menino</li>
-                            <li class="videos" data-itemid="4">The Large Hadron Collider</li>
-                        </ul>
-                    </div>
-                </div>
-
-<?php
+        include('bodyFeaturedBox.php');
         include('bodySidebar.php');
         include('bodyPostGrid.php');
 
