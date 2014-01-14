c6e0ae93411686b9c1f7926e6942bf7bed0624fc
Nick Pettazzoni <pettazz@gmail.com>
2014-01-13 22:44:48 -0500

    GOOGLE PLUS SO HARD MOTHERFUCKERS TRYNA GIVE US A VANITY URL which we graciously accepted.

--- bodySidebar.php
+++ bodySidebar.php
@@ -31,7 +31,7 @@
 
                     <ul id="social-links">
                         <li><a href="http://twitter.com/warrantynowvoid" target="_blank"><img src="/assets/img/template/aside_twitter.png" /></a></li>
-                        <li><a href="https://plus.google.com/101358114440767552212" rel="publisher" target="_blank"><img src="/assets/img/template/aside_gplus.png" /></a></li>
+                        <li><a href="https://plus.google.com/+Warrantynowvoid" rel="publisher" target="_blank"><img src="/assets/img/template/aside_gplus.png" /></a></li>
                         <li><a href="http://www.youtube.com/user/WarrantyNowVoid/" target="_blank"><img src="/assets/img/template/aside_youtube.png" /></a></li>
                         <li><a href="http://facebook.com/warrantynowvoid" target="_blank"><img src="/assets/img/template/aside_facebook.png" /></a></li>
                     </ul>
