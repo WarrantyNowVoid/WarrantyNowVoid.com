492fe7581c172bcdea2ee0e27b62778116b94bfa
Nick Pettazzoni <pettazz@gmail.com>
2015-01-27 17:30:07 -0500

    make sure we get all the parts of the post body

--- bodyTop.php
+++ bodyTop.php
@@ -42,7 +42,7 @@
         <script type="text/javascript">
             var intervalID = window.setInterval(function(){
                 $.get('http://warrantynowvoid.com/live_update.php', function(data){
-                    $('article').children('p').remove()
+                    $('article').children('p, hr').remove()
                     $(data.body).insertAfter($('article').children('h1'));
                 });
             }, 30000);
